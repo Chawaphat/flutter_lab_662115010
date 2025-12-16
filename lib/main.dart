99a7b93460b7af53b16f2742e6834085cccf5eb0
monfufuu <ica.chawaphat@gmail.com>
2025-12-16 10:44:30 +0700

    add_navigatorBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/cals.dart';
+import 'pages/contact.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,32 +10,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ICE SHOP',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Calculator App Change'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MainPage(title: 'Calculator App Change'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var price = TextEditingController();
-  var amount = TextEditingController();
-  var receiveMoney = TextEditingController();
-  double total = 0.0;
-  double change = 0.0;
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> tabs = const [HomePage(), CalculatePage(), ContactPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -41,144 +42,27 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(
-              "Change Calculator",
-              style: TextStyle(
-                fontSize: 32,
-                fontFamily: 'Snowy',
-                color: const Color.fromARGB(255, 121, 65, 206),
-                backgroundColor: const Color.fromARGB(255, 230, 218, 255),
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            Image.asset(
-              'assets/2022-Formula1-Audi-Show-Car-005-1440w.png',
-              height: 200,
-            ),
-            Image.network(
-              'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHg3eGM4OGxjcThpczVmZHlsbW5yMWU1Y3VxbXF1OTJybW9nOTdrayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/K06nw2VT3jq6Pm0BxO/giphy.gif',
-              height: 150,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: priceTextField(),
-            ),
-            const SizedBox(height: 16),
-            Padding(padding: const EdgeInsets.all(8.0), child: amountOfItems()),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: calculateButton(),
-            ),
-            const SizedBox(height: 16),
-            Padding(padding: const EdgeInsets.all(8.0), child: totalDisplay()),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: receiveMoneyTextField(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: changeCalculationButton(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: showChangeDisplay(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget priceTextField() {
-    return TextField(
-      controller: price,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Price per item',
-      ),
-      keyboardType: TextInputType.number,
-    );
-  }
-
-  Widget amountOfItems() {
-    return TextField(
-      controller: amount,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Amount of items',
-      ),
-      keyboardType: TextInputType.number,
-    );
-  }
-
-  Widget calculateButton() {
-    return ElevatedButton(
-      onPressed: () {
-        if (price.text.isNotEmpty && amount.text.isNotEmpty) {
-          double pricePerItem = double.parse(price.text);
-          int itemCount = int.parse(amount.text);
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
           setState(() {
-            total = pricePerItem * itemCount;
+            print(index);
+            _currentIndex = index;
           });
-        }
-      },
-      child: Text('Calculate Total'),
-    );
-  }
-
-  Widget totalDisplay() {
-    return Text(
-      'Total: \$${total.toStringAsFixed(2)}',
-      style: TextStyle(fontSize: 24),
-    );
-  }
-
-  Widget receiveMoneyTextField() {
-    return TextField(
-      controller: receiveMoney,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Receive Money',
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: "Calculator",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_mail),
+            label: "Contact",
+          ),
+        ],
       ),
-      keyboardType: TextInputType.number,
-    );
-  }
-
-  Widget changeCalculationButton() {
-    return ElevatedButton(
-      onPressed: () {
-        if (receiveMoney.text.isEmpty || total <= 0) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text('money is not be empty')));
-          return;
-        }
-
-        double received = double.parse(receiveMoney.text);
-
-        if (received < total) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text('money is not enough')));
-          return;
-        }
-
-        setState(() {
-          change = received - total;
-        });
-      },
-      child: Text('Calculate Change'),
-    );
-  }
-
-  Widget showChangeDisplay() {
-    return Text(
-      'Change: \$${change.toStringAsFixed(2)}',
-      style: TextStyle(fontSize: 24),
     );
   }
 }
---
+++ lib/pages/cals.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class CalculatePage extends StatefulWidget {
+  const CalculatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatePage> createState() => _CalculatePageState();
+}
+
+class _CalculatePageState extends State<CalculatePage> {
+  var price = TextEditingController();
+  var amount = TextEditingController();
+  var receiveMoney = TextEditingController();
+  double total = 0.0;
+  double change = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              "Change Calculator",
+              style: TextStyle(
+                fontSize: 32,
+                fontFamily: 'Snowy',
+                color: const Color.fromARGB(255, 121, 65, 206),
+                backgroundColor: const Color.fromARGB(255, 230, 218, 255),
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Image.asset(
+              'assets/2022-Formula1-Audi-Show-Car-005-1440w.png',
+              height: 200,
+            ),
+            Image.network(
+              'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHg3eGM4OGxjcThpczVmZHlsbW5yMWU1Y3VxbXF1OTJybW9nOTdrayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/K06nw2VT3jq6Pm0BxO/giphy.gif',
+              height: 150,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: priceTextField(),
+            ),
+            const SizedBox(height: 16),
+            Padding(padding: const EdgeInsets.all(8.0), child: amountOfItems()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: calculateButton(),
+            ),
+            const SizedBox(height: 16),
+            Padding(padding: const EdgeInsets.all(8.0), child: totalDisplay()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: receiveMoneyTextField(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: changeCalculationButton(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: showChangeDisplay(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget priceTextField() {
+    return TextField(
+      controller: price,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Price per item',
+      ),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget amountOfItems() {
+    return TextField(
+      controller: amount,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Amount of items',
+      ),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget calculateButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (price.text.isNotEmpty && amount.text.isNotEmpty) {
+          double pricePerItem = double.parse(price.text);
+          int itemCount = int.parse(amount.text);
+          setState(() {
+            total = pricePerItem * itemCount;
+          });
+        }
+      },
+      child: Text('Calculate Total'),
+    );
+  }
+
+  Widget totalDisplay() {
+    return Text(
+      'Total: \$${total.toStringAsFixed(2)}',
+      style: TextStyle(fontSize: 24),
+    );
+  }
+
+  Widget receiveMoneyTextField() {
+    return TextField(
+      controller: receiveMoney,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Receive Money',
+      ),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget changeCalculationButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (receiveMoney.text.isEmpty || total <= 0) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('money is not be empty')));
+          return;
+        }
+
+        double received = double.parse(receiveMoney.text);
+
+        if (received < total) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('money is not enough')));
+          return;
+        }
+
+        setState(() {
+          change = received - total;
+        });
+      },
+      child: Text('Calculate Change'),
+    );
+  }
+
+  Widget showChangeDisplay() {
+    return Text(
+      'Change: \$${change.toStringAsFixed(2)}',
+      style: TextStyle(fontSize: 24),
+    );
+  }
+}
